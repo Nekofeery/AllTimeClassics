eaa9ca04e5563dd7c3525d3bfa7d4d1357f67c0a
haume <Melanie.hauser@campus.lmu.de>
2021-02-03 13:54:07 +0100

    Add selection sort

--- Source/AllTimeClassics/Private/ArraySorting.cpp
+++ Source/AllTimeClassics/Private/ArraySorting.cpp
@@ -8,6 +8,8 @@ void ArraySorting<T>::InsertionSort(TArray<T> Array)
 {
 	// The value we are currently comparing to
 	int Key;
+	// The number of elements
+	int Size = Array.Num();
 	for (int i = 1; i < Size; i++)
 	{
 		Key = Array[i];
@@ -25,5 +27,33 @@ void ArraySorting<T>::InsertionSort(TArray<T> Array)
 				Array[j] = Array[j - 1];
 			}
 		}
+	}
+}
+
+template<typename T>
+void ArraySorting<T>::SelectionSort(TArray<T> Array)
+{
+	// The index of the smallest value found in the unsorted part of the array
+	int MinimumIndex = 0;
+	// The number of elements
+	int Size = Array.Num();
+	// Temporary variable for swapping values
+	int Temp = 0;
+	for (int i = 0; i < Size - 1; i++)
+	{
+		MinimumIndex = i;
+		// Go through the unsorted part
+		for (int j = i + 1; j < Size; j++)
+		{
+			// A smaller value was found
+			if (Array[j] < Array[MinimumIndex])
+			{
+				MinimumIndex = j;
+			}
+		}
+		// Swap the smallest value with the first
+		Temp = Array[MinimumIndex];
+		Array[MinimumIndex] = Array[i];
+		Array[i] = Temp;
 	}
 }
--- Source/AllTimeClassics/Public/ArraySorting.h
+++ Source/AllTimeClassics/Public/ArraySorting.h
@@ -17,4 +17,10 @@ public:
 	* @note Worst case time complexity is O(n²); useful for already mostly ordered arrays
 	*/
 	void InsertionSort(TArray<T> Array);
+	/**
+	* Selection sort
+	* @param Array - The array to sort
+	* @note Time complexity is always O(n²) - sloooow; efficient in terms of memory usage
+	*/
+	void SelectionSort(TArray<T> Array);
 };
